dfb803106d4759c4d235fb1647012a5a21df2f14
Harsh Singhal <hsnghal@amazon.com>
2020-03-06 14:26:55 +0530

    add message cell and constant file

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Singhal, Harsh on 06/03/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email id
+    let body: String
+}
